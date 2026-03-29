85886b16e3f802017fb8ffd184bebd5e55ddf9b4
Md Manowar Alam <mdmanowar0999@gmail.com>
2026-03-28 21:36:28 -0700

    third commit

---
+++ PythonFrom-BasicToAdv/5.InputFunction.py
@@ -0,0 +1,6 @@
+#taking input from user
+name=print(input("enter your name :"))
+
+a=input()
+print(int(a)+int(a))
+#input function always take input as string and return it
---
+++ PythonFrom-BasicToAdv/6.Assignment-2.py
@@ -0,0 +1,26 @@
+
+
+#Q1.write a python program to input student name and marks of 3 subject .print name &percentage inoutput
+#Name=input("Enter your name:")
+cheMarks=input("Enter your chems:")
+phyMarks=input("enter your phys:")
+mathsMarks=input("enter your maths:")
+
+#claculating percentage
+percentage=((int(cheMarks)+int(phyMarks)+int(mathsMarks))/300)*100
+
+#print("my name :",Name)
+print("percentage :",percentage)
+
+#Q2.program that collect data multiple times of data from user and store in dictionary and then print tha data
+user_data={} #intilizing a dictionary
+
+#input from user data
+user_data["name"]=input("Enter your name:")
+user_data["age"]=int(input("Enter your age:"))
+user_data["height"]=float(input("Enter your height:"))
+user_data["student"]=input("Enter your name (yes/no):")
+
+#print
+print(user_data)
+
