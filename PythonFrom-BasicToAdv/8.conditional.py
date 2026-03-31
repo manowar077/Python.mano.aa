72086801bbaaf3e3bb4a2d87e158acb7208d2599
Md Manowar Alam <mdmanowar0999@gmail.com>
2026-03-30 23:11:26 -0700

    sss

---
+++ PythonFrom-BasicToAdv/8.conditional.py
@@ -0,0 +1,22 @@
+#conditional
+#1. If-Else Statement
+a=26
+b=100
+if a>b:
+    print("a is greater than b")
+else:
+    print("a is less than b")
+
+#2.if elif else
+if a>b:
+    print("a is greater than b")
+elif a<b:
+        print("a is less than b")
+else:
+        print("a is greater than or equal to b")
+
+#ternary operator
+age=16
+status="adult" if age >= 18 else "teen"
+print(age)
+print(status)
