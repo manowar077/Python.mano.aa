fe6934b75a0b8d1f48d29d78372a9b71799ed63b
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-09 21:40:18 +0530

    update

---
+++ PythonFrom-BasicToAdv/9.Stringsss.py
@@ -0,0 +1,36 @@
+# string in python
+str1="my name is md manowar alam"
+str2="computer sciencE"
+print(str1)
+print(str2)
+print(str1.upper())
+print(str2.lower())
+
+#concatenation (Means adding two string)
+sr1="my name is "
+sr2="md manowar alam"
+print(sr1+sr2)
+
+#string length
+srt4="MD manowar alam"
+print(len(srt4))
+
+#indexing
+df="manowar"
+print(df[2])
+
+#slicing
+rrr="Manowar"
+print(rrr[1:4])
+print(rrr[:4])
+print(rrr[0:])
+print(rrr[:])
+print(rrr[-3:-0])
+
+#str.format()
+name="manowar"
+age=21
+print("My name is {} ".format(name))
+print("age is {} ".format(age))
+
+
