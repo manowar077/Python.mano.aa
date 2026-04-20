6340c289ef73d4f1ab579a1545b1be7633fa5400
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-20 09:56:00 +0530

    update

--- PythonFrom-BasicToAdv/11.Argument.py
+++ PythonFrom-BasicToAdv/11.Argument.py
@@ -3,4 +3,25 @@ def print_info(name="xyz", age=21):
 
 # order of argument
 # 1.postional -> Default -> positional variable -> keyword variable length
+#1.Required Argument
+def greeting(namee):
+    print("hek=llo",namee ,"!")
+greeting("madav")
 
+#2.Default Argumeent
+
+def greeting(name="manowar"):
+    print("hello",name ,"!")
+greeting("alam")
+
+#3.Keyword Argument
+def divide(a,b):
+    return a/b
+result=divide(10,5)
+print(result)
+
+#4.Arbitary argument
+def add_num(*args):
+    return sum(args)
+result=add_num(1,2,3,4,5)
+print(result)
