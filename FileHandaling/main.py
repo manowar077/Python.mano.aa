079b42a1ae0c17fa18456fac1a29e21c97f40da5
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-21 09:37:52 +0530

    update

--- FileHandaling/main.py
+++ FileHandaling/main.py
@@ -1,5 +1,11 @@
+#Open In Read Mode
 file=open("sample.txt","r")
-data=file.read()
+data=file.read(5) #we defined a size
 print(data)
 print(type(data))
+dat=file.readline()
+print(dat)
+dat=file.readline()
+print(dat)
 file.close()
+#
