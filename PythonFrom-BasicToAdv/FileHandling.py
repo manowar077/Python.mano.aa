30225218a7302d19d2c07f02cd0c126e36127f15
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-23 16:50:44 +0530

    update

--- PythonFrom-BasicToAdv/FileHandling.py
+++ PythonFrom-BasicToAdv/FileHandling.py
@@ -2,8 +2,13 @@
 #     content = file.read()
 #     print(content),
 
-file = open("sample.txt")
-print.write("hello world")
-print.write("file handaling in python")
-file.close()
+# file = open("sample.txt")
+# print.write("hello world")
+# print.write("file handaling in python")
+# file.close()
 
+file=open("sample.txt","r")
+print(file.read())
+file.seek(4)
+print(file.read())
+file.close()
