cf05bf361e0d66383800ad0ce3228a945fb7fda2
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-23 16:35:52 +0530

    updaate

--- PythonFrom-BasicToAdv/dictionary.py
+++ PythonFrom-BasicToAdv/dictionary.py
@@ -4,3 +4,10 @@ print(Mydict["name"])
 print(Mydict["age"])
 print(Mydict.keys())
 print(Mydict.values())
+
+
+dict1={"a":1,"b":2,"c":3}
+dict2={"c":4,"d":5,"e":6}
+
+dict.update(dict2)
+print(dict)
--- PythonFrom-BasicToAdv/module.py
+++ PythonFrom-BasicToAdv/module.py
@@ -1,3 +1,4 @@
+
 def greet(name):
     return "hello "+name
 print(greet("manowar"))
