f8774cdb6397c3408a7a2c1995b4fd59218de5cf
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-23 16:54:22 +0530

    update

--- PythonFrom-BasicToAdv/FileHandling.py
+++ PythonFrom-BasicToAdv/FileHandling.py
@@ -6,9 +6,21 @@
 # print.write("hello world")
 # print.write("file handaling in python")
 # file.close()
-
+ #seek
 file=open("sample.txt","r")
 print(file.read())
 file.seek(4)
 print(file.read())
+file.close()
+
+#tell and seek
+file=open("sample.txt","r")
+print(file.tell())
+
+file.read(10)
+print(file.tell())
+
+file.seek(0)
+print(file.tell())
+
 file.close()
