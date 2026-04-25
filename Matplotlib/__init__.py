1940f10d15a78e9629add374657122c606947bcd
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-25 14:38:18 +0530

    up

---
+++ Matplotlib/__init__.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+# Plot
+plt.plot(x, y)
+
+# Labels and title
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Simple Line Plot")
+
+# Show plot
+plt.show()
