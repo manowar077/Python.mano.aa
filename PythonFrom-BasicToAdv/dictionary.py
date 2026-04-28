5157e0af7c69c70afb6e49d8b296d05b7784b82d
Md Manowar Alam <mdmanowar077@gmail.com>
2026-04-28 09:42:02 +0530

    up

--- PythonFrom-BasicToAdv/dictionary.py
+++ PythonFrom-BasicToAdv/dictionary.py
@@ -11,3 +11,13 @@ dict2={"c":4,"d":5,"e":6}
 
 dict.update(dict2)
 print(dict)
+
+a=[]
+for i in range(1,100):
+    if(i%2==0):
+        a.append(i)
+print(a)
+
+a=[i**2 for i in range(1,100) if i%2==0]
+print(a)
+
